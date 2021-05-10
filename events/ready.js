22374c61ff82d3e8f28b78978a40410e96935876
ALone <workforgodspray@gmail.com>
2021-05-10 09:38:45 +0530

    Update ready.js
    Co-Authored-By: ALone <workforgodspray@gmail.com>

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   client.Ready = true;
-  client.user.setActivity("Music", { type: "LISTENING" }).then(() => {
+  client.user.setActivity("Music on EYE ESPORTS", { type: "PLAYING" }).then(() => {
     client.Manager.init(client.user.id);
     client.log("Successfully Logged in as " + client.user.tag);
   });
